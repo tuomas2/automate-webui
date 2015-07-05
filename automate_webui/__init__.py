2fd227fc6a9ebff2f8d9c8b21571d4e43a2511a5
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2015-07-05 15:40:47 +0300

    Edit licence note in all files

--- automate_webui/__init__.py
+++ automate_webui/__init__.py
@@ -1,20 +1,25 @@
 # -*- coding: utf-8 -*-
 # (c) 2015 Tuomas Airaksinen
 #
-# This file is part of automate_webui.
+# This file is part of automate-webui.
 #
-# automate_webui is free software: you can redistribute it and/or modify
+# automate-webui is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# automate_webui is distributed in the hope that it will be useful,
+# automate-webui is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with automate_webui.  If not, see <http://www.gnu.org/licenses/>.
+# along with automate-webui.  If not, see <http://www.gnu.org/licenses/>.
+#
+# ------------------------------------------------------------------
+#
+# If you like Automate, please take a look at this page:
+# http://tuomasairaksinen.fi/automate/gospel/
 
 __author__ = "Tuomas Airaksinen"
 __copyright__ = "Copyright 2015, Tuomas Airaksinen"
